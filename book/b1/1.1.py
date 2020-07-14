36c28782f7dd8e397ea24c39930813d0d4039c31
홍지성 <dgf5820@daum.net>
2020-07-14 17:42:30 +0900

    # Update - 1.1 정수 예제 학습 완료

--- book/b1/1.1.py
+++ book/b1/1.1.py
@@ -1,1 +1,15 @@
-# 정수와 복소수
+# 1.1 정수
+
+# 10진법
+# 문자열 -> 숫자
+number1 = int('11')
+print('number1 :', number1)
+
+# 2진법 -> 10진법
+# 문자열 -> 숫자
+number2 = int('11', 2)
+print('number2 :', number2)
+
+# 정수를 나타내는데 필요한 바이트
+byte = (999).bit_length()
+print('byte :', byte)
